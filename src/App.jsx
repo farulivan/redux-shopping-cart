6ba11e4645a479d0727498e3f748c8950980901d
Farul Ivan <farulivan@gmail.com>
2022-11-08 10:56:03 +0700

    add axios and fetch data from api instead of local data

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import { CartContainer } from "./components/CartContainer";
 import Navbar from "./components/Navbar";
 import { useDispatch, useSelector } from "react-redux";
-import { calculateTotals } from "./features/cart/cartSlice";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
 import { useEffect } from "react";
 import { Modal } from "./components/Modal";
 
 function App() {
   const dispatch = useDispatch();
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
 
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <main>
